82e08ba2a0f874d976fd1c44a2bf2ac984c94eaf
SkecisAI <skecis@qq.com>
2019-12-02 11:13:42 +0800

    修改lagou

--- lagou.py
+++ lagou.py
@@ -5,6 +5,7 @@ import jieba
 import requests
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib as mpl
 from urllib.parse import quote
 from bs4 import BeautifulSoup
@@ -19,6 +20,7 @@ mpl.rcParams['font.sans-serif'] = ['SimHei']
 class JobSearch:
     def __init__(self, kd):
         self.key_word = kd
+        self.__job_count = 0
         self.get_url = "https://www.lagou.com/jobs/list_%s?labelWords=sug&fromSearch=true&suginput=%s" % \
                        (str(quote(kd)), str(quote(kd)))
         self.get_headers = {
@@ -36,9 +38,11 @@ class JobSearch:
 
     def job_info_crawl(self):
         s = requests.Session()
-        s.get(self.get_url, headers=self.get_headers)
+        r = s.get(self.get_url, headers=self.get_headers)
+        soup = BeautifulSoup(r.content, "lxml")
+        page_nums = int(soup.find('span', attrs={'class': 'span totalNum'}).text)
         position_df = pd.DataFrame()
-        for i in range(1, 25+1):  # 爬取25页职位信息
+        for i in range(1, page_nums):  # 爬取25页职位信息
             print("-----正在爬取第%d页-----" % i)
             post_data = {
                 'first': 'true',
@@ -46,7 +50,8 @@ class JobSearch:
                 'kd': self.key_word
             }
             # if (i % 10) == 0:
-            print('新会话')
+            print('<--新会话-->')
+            time.sleep(np.random.randint(0, 1))  # 间隔0-1s
             s = requests.Session()
             s.get(self.get_url, headers=self.get_headers)
             p = s.post(self.post_rul, data=post_data, headers=self.post_headers)
@@ -54,7 +59,7 @@ class JobSearch:
             show_id = p_json['content']['showId']  # 具体信息页面id-1
             job_info = p_json['content']['positionResult']['result']  # 职位简要信息
             rp, rq = [], []
-            for j in range(10):
+            for j in range(15):
                 if (i == 1) and (j == 0):
                     position_df = pd.DataFrame(columns=job_info[j].keys())
                 position_df.loc[position_df.shape[0]] = job_info[j]
@@ -67,14 +72,15 @@ class JobSearch:
                 rp.extend(text_tuple[0])
                 rq.extend(text_tuple[1])
             self.__save_to_txt(rp, rq)  # 存储工作要求详情, 读写一页
-        # self.__save_to_csv(position_df)  # 存储工作信息
+        self.__save_to_csv(position_df)  # 存储工作信息
+        print("一共爬取了%d条岗位信息" % self.__job_count)
 
     def generate_wordcloud(self):
         """
         绘制词云
         """
-        f_rp = open('responsibility.txt', 'r', encoding='utf-8').read()
-        f_rq = open('requirement.txt', 'r', encoding='utf-8').read()
+        f_rp = open(self.key_word + '_responsibility.txt', 'r', encoding='utf-8').read()
+        f_rq = open(self.key_word + '_requirement.txt', 'r', encoding='utf-8').read()
         cut_text_rp = " ".join(jieba.cut(f_rp))
         cut_text_rq = " ".join(jieba.cut(f_rq))
         cut_text_rq = re.sub(r'\+\+', 'pp', cut_text_rq)
@@ -92,19 +98,25 @@ class JobSearch:
 
         plt.subplot(121)
         plt.imshow(word_cloud_rp, interpolation='bilinear')
-        plt.title("工作职责")
+        plt.title(self.key_word + "工作职责")
         plt.axis('off')
 
         plt.subplot(122)
         plt.imshow(word_cloud_rq, interpolation='bilinear')
-        plt.title("岗位需求")
+        plt.title(self.key_word + "岗位需求")
         plt.axis('off')
 
         plt.show()
 
     def __job_detail_crawl(self, r):
         soup = BeautifulSoup(r.content, 'lxml')
-        job_detail = soup.find('div', attrs={'class': 'job-detail'}).find_all('p')
+        # job_detail = None
+        try:
+            job_detail = soup.find('div', attrs={'class': 'job-detail'}).find_all('p')
+        except AttributeError:
+            print('警告！发生重定向。')
+            return [], []
+        self.__job_count += 1
         info_id = 0
         respon_list, reques_list = [], []
         for d in job_detail:
@@ -120,34 +132,88 @@ class JobSearch:
         return respon_list, reques_list  # 工作职责信息， 任职要求信息
 
     def __save_to_txt(self, rp, rq):
-        with open('responsibility.txt', 'a+', encoding='utf-8', ) as f:  # 存储工作职责
+        # 创建文件，若存在则清空内容
+        with open(self.key_word + '_responsibility.txt', 'w', encoding='utf-8', ) as f:
+            f.close()
+        with open(self.key_word + '_requirement.txt', 'w', encoding='utf-8') as f:
+            f.close()
+
+        with open(self.key_word + '_responsibility.txt', 'a+', encoding='utf-8', ) as f:  # 存储工作职责
             for p in rp:
                 f.write(p)
             f.close()
-        with open('requirement.txt', 'a+', encoding='utf-8') as f:  # 存储任职要求
+        with open(self.key_word + '_requirement.txt', 'a+', encoding='utf-8') as f:  # 存储任职要求
             for q in rq:
                 f.write(q)
             f.close()
 
     def __save_to_csv(self, df):
-        df.to_csv("machine_learning_job_info.csv", encoding='gb18030', index=False)
+        df.to_csv(self.key_word + "_job_info.csv", encoding='gb18030', index=False)
 
     def handle_data(self):
-        position_df = pd.read_csv('machine_learning_job_info.csv', encoding='gb18030')
+        position_df = pd.read_csv(self.key_word + '_job_info.csv', encoding='gb18030')
         unused_col = ['explain', 'plus', 'gradeDescription', 'promotionScoreExplain']
         position_df.drop(columns=unused_col, inplace=True)
         unimportant_col = ['businessZones', 'subwayline', 'stationname', 'linestaion',
                            'positionId', 'companyId', 'companyFullName', 'companyLogo', 'createTime', 'formatCreateTime', 'district',
-                           'lastLogin', 'publisherId']
+                           'lastLogin', 'publisherId', 'industryLables']
         position_df.drop(columns=unimportant_col, inplace=True)
-        position_df['welfare'] = position_df.apply(lambda x: set(x['companyLabelList'].extend(x['positionAdvantage'])), axis=1)
-        position_df.drop(columns=['companyLabelList', 'positionAdvantage'], inplace=True)
 
-        print(position_df.info())
+        position_df['welfare'] = position_df.apply(lambda x: x['companyLabelList'] + x['positionAdvantage'], axis=1)
+        position_df['label'] = position_df.apply(lambda x: x['skillLables'] + x['positionLables'], axis=1)
+        position_df['salary'] = position_df['salary'].map(lambda x: re.compile('([0-9]+)k').findall(x))
+        position_df['salary_min'] = position_df.apply(lambda x: x['salary'][0], axis=1).astype('int64')
+        position_df['salary_max'] = position_df.apply(lambda x: x['salary'][-1], axis=1).astype('int64')
+        position_df['salary_ave'] = position_df.apply(lambda x: (x['salary_min'] + x['salary_max']) / 2, axis=1)
+        position_df.drop(columns=['companyLabelList', 'positionAdvantage', 'salary'], inplace=True)
+
+        def salary_cat(x):
+            if x <= 20:
+                return "20k以下"
+            elif (x >= 20) and (x <= 30):
+                return "20k~30k"
+            else:
+                return "30k以上"
+
+        position_df['薪资等级'] = position_df['salary_ave'].map(salary_cat)
+
+        plt.subplot(221)
+        plt.hist(position_df['salary_ave'])
+        plt.title(self.key_word + '岗位月薪分布')
+        plt.xlabel('月薪（k）')
+        plt.ylabel('职位数量（个）')
+
+        label = " ".join([lab for lab in position_df['label']])
+        cut_text = " ".join(jieba.cut(label))
+        word_cloud = WordCloud(
+            scale=5,
+            font_path='C:/Windows/Fonts/simfang.ttf',
+            background_color="white", width=1000, height=1000
+        ).generate(cut_text)
+
+        plt.subplot(222)
+        plt.imshow(word_cloud, interpolation='bilinear')
+        plt.title(self.key_word + '应用关键字')
+        plt.axis('off')
+
+        plt.subplot(223)
+        sns.countplot(x=position_df['education'], hue=position_df['薪资等级'])
+        plt.xlabel('最低学历要求')
+        plt.ylabel('职位数量（个）')
+        plt.title(self.key_word + '岗位学历要求以及薪资')
+
+        plt.subplot(224)
+        sns.countplot(x=position_df['workYear'], hue=position_df['薪资等级'])
+        plt.xlabel('工作经验')
+        plt.ylabel('职位数量（个）')
+        plt.title(self.key_word + '岗位工作经验以及薪资')
+
+        plt.show()
 
 
 if __name__ == '__main__':
-    job_search = JobSearch('机器学习')
-    # job_search.job_info_crawl()
+    user_word = input("输入搜索关键字： ")
+    job_search = JobSearch(user_word)
+    job_search.job_info_crawl()
     job_search.generate_wordcloud()
-    # job_search.handle_data()
+    job_search.handle_data()
